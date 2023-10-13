93de59ddd26c4721988156089bdb5cba86e5083a
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:17:21 -0300

    Create ft_div_mod.c

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 15:18:15 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 15:09:44 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 10;
+	b = 5;
+	ft_div_mod(a, b, &div, &mod);
+	printf("divisão = %d \t resto = %d", div, mod);
+}
