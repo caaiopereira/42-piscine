36acbb1829680fe90257c6872f52a90ced9b9f79
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:22:02 -0300

    Create ft_strncpy.c

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 19:58:28 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 15:53:44 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int	main()
+{
+	char	src[] = "abc";
+	char	dest[] = "";
+
+	printf("%s", ft_strncpy(dest, src, 4));
+}
