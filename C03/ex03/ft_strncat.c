55f0fdb58c0f77c781a40bc1831b33d0587a06c3
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:31:18 -0300

    Create ft_strncat.c

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 09:36:47 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 16:39:34 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	x;
+
+	i = 0;
+	x = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (x < nb && src[x] != '\0')
+	{
+		dest[i + x] = src[x];
+		x++;
+	}
+	dest[i + x] = '\0';
+	return (dest);
+}
+
+int	main()
+{
+	char	src[] = "rio";
+	char	dest[] = "42";
+
+	printf("%s", ft_strncat(dest, src, 5));
+}
