93ea2479999f713d854969e46df2e9332c0b83c3
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:42:32 -0300

    Create ft_fibonacci.c

---
+++ C05/ex03/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 15:23:35 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 06:55:34 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	if (index < 2)
+	{
+		return (index);
+	}
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
+
+/*int	main(void)
+{
+	printf("%d", ft_fibonacci(7));
+}*/
