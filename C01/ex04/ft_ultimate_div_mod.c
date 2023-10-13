e4192decf257bae91128f5409ab5a03662c1e9f3
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:17:49 -0300

    Create ft_ultimate_div_mod.c

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,37 @@
+/************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 16:37:23 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 15:13:05 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 10;
+	b = 5;
+	ft_ultimate_div_mod(&a, &b);
+	printf("divisão = %d \t resto = %d", a, b);
+}
