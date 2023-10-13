e5059161561c49decff027211d30d8b890d9d792
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:40:11 -0300

    Create ft_recursive_power.c

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 14:21:28 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 06:54:37 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power == 0)
+	{
+		return (1);
+	}
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power > 1)
+	{
+		return (nb * ft_recursive_power(nb, (power -1)));
+	}
+	return (i);
+}
+
+/*int	main(void)
+{
+	printf("%d", ft_recursive_power(3, 4));
+}*/
