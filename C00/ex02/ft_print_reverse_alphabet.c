38eb1d2a29c4c910f8a2a152972d08a9c4aa1c63
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:10:10 -0300

    Create ft_print_reverse_alphabet.c

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:31:07 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 10:12:21 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
