a642d2c38fb7c0266999b80f5dc527e5255a5fe0
caaiopereira <caaiopereira9@gmail.com>
2023-10-13 13:38:01 -0300

    Create ft_iterative_power.c

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 14:05:20 by caiperei          #+#    #+#             */
+/*   Updated: 2023/08/17 07:02:41 by caiperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	while (power > 1)
+	{
+		i = i * nb;
+		power--;
+	}
+	return (i);
+}
+
+/*int	main(void)
+{
+	printf("%d", ft_iterative_power(3, 4));
+}*/
